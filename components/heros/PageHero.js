8f6e050238f8fa21ec3e64def7fb47266ea105a0
esmith7196 <ericsmith.100@gmail.com>
2023-08-26 16:31:24 -0400

    Change font size of small dev pagehero title

--- components/heros/PageHero.js
+++ components/heros/PageHero.js
@@ -25,7 +25,7 @@ const PageHero = props => {
             </Link>
           </div>
         )}
-        <h1 className="relative  text-5xl md:text-7xl xl:text-8xl text-white font-display  font-bold flex justify-center items-center uppercase mt-16 lg:mt-52  text-center">
+        <h1 className="relative  text-5xl sm:text-7xl xl:text-8xl text-white font-display  font-bold flex justify-center items-center uppercase mt-16 lg:mt-52  text-center">
           {props.pageTitle}
         </h1>
       </div>
