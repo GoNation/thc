8b2eeab40ba9d1f95bb0771316b752b408912989
esmith7196 <ericsmith.100@gmail.com>
2023-08-29 16:23:48 -0400

    Remove svg's

--- pages/index.js
+++ pages/index.js
@@ -105,7 +105,6 @@ export default function Home({
           />
 
           <LargeContentContainer story={findStoryByName('Homepage story 5')} />
-          <img src="/wedding.svg" alt="" />
         </section>
       </Layout>
     </AppContext.Provider>
