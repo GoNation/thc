f20371db631719fca1ba40adccfcd0edc8a4c0a1
esmith7196 <ericsmith.100@gmail.com>
2023-08-29 17:45:07 -0400

    Pricing warning fix

--- components/menu/Price.js
+++ components/menu/Price.js
@@ -2,20 +2,18 @@ import React from 'react';
 
 const Price = ({ variants, withDollar }) => {
   return (
-    <span className="menuPriceContainer text-white pr-4 block">
-      {variants[0].labelTitle ? (
+    <div className="menuPriceContainer text-white pr-4 block">
+      {variants[0].labelTitle && (
         <p className="menuItemPriceLabel font-bold">{variants[0].labelTitle}</p>
-      ) : (
-        ''
       )}
       {variants[0].labelTitle && (
         <p className="menuItemPriceLabel font-bold">{variants[0].label}</p>
-      )}{' '}
+      )}
       <p className="menuItemPrice">
         {withDollar ? '$' : ''}
         {variants[0].price}
       </p>
-    </span>
+    </div>
   );
 };
 
