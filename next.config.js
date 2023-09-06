860e0a3cfe10e7ab3f6824062ddab1732435f450
esmith7196 <ericsmith.100@gmail.com>
2023-09-06 11:36:58 -0400

    Handle redirects

--- next.config.js
+++ next.config.js
@@ -13,6 +13,75 @@ const nextConfig = {
     });
     return cfg;
   },
+  async redirects() {
+    return [
+      {
+        source: '/full-specialty-drink-menu/',
+        destination: '/drinks',
+        permanent: true,
+      },
+      {
+        source: '/full-specialty-drink-menu',
+        destination: '/drinks',
+        permanent: true,
+      },
+      {
+        source: '/Specialty-Drinks-Wine-List',
+        destination: '/drinks',
+        permanent: true,
+      },
+      {
+        source: '/Spirit-Section',
+        destination: '/drinks',
+        permanent: true,
+      },
+      {
+        source: '/Happy-Hour',
+        destination: '/menu',
+        permanent: true,
+      },
+      {
+        source: 'whiteclamwednesday/',
+        destination: '/events',
+        permanent: true,
+      },
+      {
+        source: '/mug-club/',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/mug-club',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/Trivia-Night-White-Clam-Wednesday',
+        destination: '/events',
+        permanent: true,
+      },
+      {
+        source: '/Penny-Brunch',
+        destination: '/menu',
+        permanent: true,
+      },
+      {
+        source: '/Live-Music-Entertainment',
+        destination: '/events',
+        permanent: true,
+      },
+      {
+        source: '/Special-Events-The-Beer-Garden',
+        destination: '/events',
+        permanent: true,
+      },
+      {
+        source: '/home/book-a-party',
+        destination: '/private-events-and-parties',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
